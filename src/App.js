4f5a90cd30f5b4467d9be07b5d70c9a74a6f843a
Rebecca Bakels <rebeccabakels@gmail.com>
2021-03-02 12:18:49 -0500

    flex and grid

--- src/App.js
+++ src/App.js
@@ -11,9 +11,20 @@ function App() {
         <button className="navigation-btn" href="/js/">JavaScript</button > |
         <button className="navigation-btn" href="/python/">Python</button >
       </nav>
-      <div className='container' >
+      <div className='containe r' >
         <div className="container-element_round" >element1 </div>
         <div className="container-element_square"> element2</div>
+      </div>
+      <div class="grid-container">
+          <div class="grid-item">1</div>
+          <div class="grid-item">2</div>
+          <div class="grid-item">3</div>
+          <div class="grid-item">4</div>
+          <div class="grid-item">5</div>
+          <div class="grid-item">6</div>
+          <div class="grid-item">7</div>
+          <div class="grid-item">8</div>
+          <div class="grid-item">9</div>
       </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
