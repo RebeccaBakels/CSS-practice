e0c4077f8282ce7f119e78f4f2f203a6efa17b2b
Rebecca Bakels <rebeccabakels@gmail.com>
2021-03-02 11:49:52 -0500

    scss practice

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+      <nav className="navigation" >
+        <button className="navigation-btn" href="/html/">HTML</button > |
+        <button className="navigation-btn" href="/css/">CSS</button > |
+        <button className="navigation-btn" href="/js/">JavaScript</button > |
+        <button className="navigation-btn" href="/python/">Python</button >
+      </nav>
+      <div className='container' >
+        <div className="container-element_round" >element1 </div>
+        <div className="container-element_square"> element2</div>
+      </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +28,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer>
+        <h3>Footer</h3>
+        <button>New Button</button>
+      </footer>
     </div>
   );
 }
